eefde05bce9eaa217b94f27663e7ca7abd130b3b
HarishBirlangi <birlangiharish1495@gmail.com>
2022-10-28 18:14:54 +0530

    adding sqflite

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,18 @@
+
+class DatabaseAlredyOpenException implements Exception {}
+
+class UnableToGetDocumnetsDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExist implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNotes implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
